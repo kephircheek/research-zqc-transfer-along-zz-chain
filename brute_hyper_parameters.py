0a9a01eae7859b731a6b66f308da7a10ad32edfb
Ilia Lazarev <kephircheek@gmail.com>
2023-03-27 08:26:48 +0300

    Brute ex < n_sender cases

--- brute_hyper_parameters.py
+++ brute_hyper_parameters.py
@@ -14,29 +14,68 @@ from quanty import matrix
 from quanty.basis import ComputationBasis
 from quanty.geometry import ZigZagChain
 from quanty.hamiltonian import XXZ
+from quanty.matrix.norm import frobenius_norm
 from quanty.model.homo import Homogeneous
+from quanty.problem.transfer import TransferAlongChain, TransferZQCAlongChain
 from quanty.state.coherence import coherence_matrix
-from quanty.task.transfer_ import TransferAlongChain, TransferZQCPerfectlyTask
+from quanty.task.transfer_ import FitTransferZQCPerfectlyTask, TransferZQCPerfectlyTask
 
 from config import PATH_DIR_DATA
 
 
-def calc_case(path, length, width, h_angle, tt, norm, n_sender):
+def loss_frobenius(m):
+    """Return loss function based on Frobenius norm (true)"""
+    m_inf = np.zeros_like(m)
+    m_inf[-1, -1] = 1
+    return frobenius_norm(m - m_inf)
+
+
+def calc_case(
+    path,
+    length,
+    width,
+    h_angle,
+    tt,
+    norm,
+    n_sender,
+    n_ancillas=0,
+    excitations=None,
+    loss_function=None,
+    method=None,
+    method_kwargs=None,
+):
     geometry = ZigZagChain.from_two_chain(2, width)  # two is True
     model = Homogeneous(geometry, h_angle=h_angle, norm_on=(0, 1) if norm else None)
     hamiltonian = XXZ(model)
-    problem = TransferAlongChain.init_classic(
+    problem = TransferZQCAlongChain.init_classic(
         hamiltonian,
         length=length,
         n_sender=n_sender,
-        excitations=n_sender,
+        excitations=n_sender if excitations is None else excitations,
+        n_ancillas=0,
     )
-    task = TransferZQCPerfectlyTask(problem, transmission_time=tt)
-    result = task.run()
+    transfer_task = TransferZQCPerfectlyTask(problem, transmission_time=tt)
+    if loss_function is None:
+        result = task.run()
+    else:
+        fit_transfer_task = FitTransferZQCPerfectlyTask(
+            transfer_task,
+            loss_function=loss_function,
+            method=method,
+            method_kwargs=method_kwargs,
+            polish=True,
+            fsolve=True,
+            history_maxlen=1,
+        )
+
+        fit_transfer_result = fit_transfer_task.run()
+        result = fit_transfer_result.history[-1]
+
     quanty.json.dumps(result, indent=2)
     path.write_text(quanty.json.dumps(result, indent=2))
 
 
+@timeti.profiler
 def calc(
     path_dir,
     length,
@@ -45,6 +84,11 @@ def calc(
     tt_span,
     norm,
     n_sender=3,
+    n_ancillas=0,
+    excitations=None,
+    loss_function=None,
+    method=None,
+    method_kwargs=None,
     n_jobs=1,
     backend="loky",
 ):
@@ -52,7 +96,18 @@ def calc(
     cases = itertools.product(width_span, h_angle_span, tt_span)
     Parallel(n_jobs=n_jobs, backend=backend)(
         delayed(calc_case)(
-            path_dir / f"{i}.json", length, width, h_angle, tt, norm, n_sender=n_sender
+            path_dir / f"{i}.json",
+            length,
+            width,
+            h_angle,
+            tt,
+            norm,
+            n_sender=n_sender,
+            excitations=excitations,
+            n_ancillas=n_ancillas,
+            loss_function=loss_function,
+            method=method,
+            method_kwargs=method_kwargs,
         )
         for i, (width, h_angle, tt) in tqdm.tqdm(list(enumerate(cases)), ncols=90)
     )
@@ -89,6 +144,12 @@ if __name__ == "__main__":
     parser.add_argument("--width", type=parse_slice, default=None)
     parser.add_argument("--angle", type=parse_slice, default=None)
     parser.add_argument("--sender", type=int, default=3)
+    parser.add_argument("--n_ancillas", type=int, default=0)
+    parser.add_argument("--excitations", type=int, default=None)
+    parser.add_argument(
+        "--method", choices=["brute_random", "dual_annealing"], default=None
+    )
+    parser.add_argument("--method_kwargs", type=str, default=None)
 
     args = parser.parse_args()
 
@@ -125,11 +186,28 @@ if __name__ == "__main__":
         ab = args.angle.stop or ab_default
         an = args.angle.step or int(abs(aa - ab) * 10) + 1
 
+    method_kwargs = json.loads(args.method_kwargs) if args.method_kwargs else None
+    excitations = args.excitations if args.excitations is not None else args.n_sender
+
     tt_suffix = f"{dump_slice(ta, tb, tn)}"
     width_suffix = f"{dump_slice(wa, wb, wn)}"
     angle_suffix = f"{dump_slice(aa, ab, an)}"
     norm_suffix = "-norm" if args.norm else ""
-    path_name = f"n{args.length}-tt_{tt_suffix}-w_{width_suffix}-a_{angle_suffix}{norm_suffix}-s{args.sender}"
+    n_ancillas_suffix = f"-na_{args.n_ancillas}" if args.n_ancillas > 0 else ""
+    excitation_suffix = f"-ex{args.excitations}" if args.excitations is None else ""
+    method_suffix = f"-m_{args.method}" if args.method else ""
+    path_name = (
+        f"n{args.length}"
+        f"-tt_{tt_suffix}"
+        f"-w_{width_suffix}"
+        f"-a_{angle_suffix}"
+        f"-s{args.sender}"
+        f"-ex{excitations}"
+        f"{excitation_suffix}"
+        f"{n_ancillas_suffix}"
+        f"{method_suffix}"
+        f"{norm_suffix}"
+    )
 
     path_dir_data = PATH_DIR_DATA / pathlib.Path(__file__).stem
 
@@ -148,6 +226,11 @@ if __name__ == "__main__":
                 h_angle_span,
                 tt_span,
                 n_sender=args.sender,
+                excitations=args.excitations,
+                loss_function=loss_frobenius,
+                n_ancillas=args.n_ancillas,
+                method=args.method,
+                method_kwargs=method_kwargs,
                 norm=args.norm,
                 n_jobs=args.n_jobs,
                 backend=args.backend,
